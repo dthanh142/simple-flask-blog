6969d3cb9a967b9cd63725503e5e9ffaa31333d3
Phạm Đức Thành (VinSmart-KHT-BPCNTT) <thanhpd8@vingroup.net>
2019-01-05 14:16:05 +0700

    add register user function

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from app import app
+from app import app, db
+from app.loginForm import RegistrationForm
 from flask import render_template, flash, redirect, url_for, request
 from werkzeug.urls import url_parse
 from app.loginForm import LoginForm
@@ -53,3 +54,16 @@ def logout():
     return redirect(url_for('index'))
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Successfully registered')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
