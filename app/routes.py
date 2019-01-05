64d6e91c9cc80e76855c6f5f30a2433bd87f7f69
Phạm Đức Thành (VinSmart-KHT-BPCNTT) <thanhpd8@vingroup.net>
2019-01-05 16:06:12 +0700

    add last seen on user profile

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,7 @@ from werkzeug.urls import url_parse
 from app.loginForm import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
+from datetime import datetime
 
 
 @app.route('/')
@@ -77,3 +78,11 @@ def user(username):
         { 'author': user, 'body': 'What a noice day' }
     ]
     return render_template('user.html', user=user, posts=posts)
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+        
